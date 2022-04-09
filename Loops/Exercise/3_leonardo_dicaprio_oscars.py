87f9d6323dafcd8cdef5b0628ba90e5e3be10c39
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-04-09 12:23:44 +0300

    Adding exercise 3

---
+++ Loops/Exercise/3_leonardo_dicaprio_oscars.py
@@ -0,0 +1,13 @@
+num = int(input())
+
+if num == 88:
+    print("Leo finally won the Oscar! Leo is happy")
+elif num < 88:
+    if num != 86:
+        print("When will you give Leo an Oscar?")
+    else:
+        print("Not even for Wolf of Wall Street?!")
+else:
+    print("Leo got one already!")
+
+
