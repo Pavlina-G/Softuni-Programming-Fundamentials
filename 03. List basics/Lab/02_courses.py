de5946b0619bb1b1b10bc765c351354b70457eb6
Pavlina-G <102150683+Pavlina-G@users.noreply.github.com>
2022-04-11 17:42:00 +0300

    Adding lab task 2

---
+++ 03. List basics/Lab/02_courses.py
@@ -0,0 +1,11 @@
+number = int(input())
+# list = []
+courses = list()
+
+for i in range(number):
+    # course = input()
+    # list.append(course)
+    courses.append(input())
+
+# print(list)
+print(courses)
